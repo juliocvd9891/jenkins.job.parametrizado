4fb82097f20d8090e7c7629833f01bf567f77f84
juliocvd9891 <julio.vargas.condor@gmail.com>
2025-04-21 12:39:23 -0600

    Update parametrizadoDSL.groovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -1,40 +1,41 @@
-job('ejemplo2-job-DSL') {
-	description('Job DSL de ejemplo para el curso de Jenkins')
-  	scm {
-      		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-        		node / gitConfigName('macloujulian')
-        		node / gitConfigEmail('macloujulian@gmail.com')
-      		}
-    	} 
+job('ejemplo2-job-DSL'){
+	description('job DSL de ejemplo para el curso de Jenkins')	
+    scm {
+      git('https://github.com/juliocvd9891/jenkins.job.parametrizado.git', 'main') { node ->
+        node / gitConfigName('juliocvd9891')
+        node / gitConfigEmail('julio.vargas.condor@gmail.com')
+      }
+    }
   	parameters {
-   		stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
-      		choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
-      		booleanParam('agente', false)
-    	}
-  	triggers {
-    		cron('H/7 * * * *')
-    	}
-  	steps {
-    		shell("bash jobscript.sh")
-    	}
-  	publishers {
-      		mailer('macloujulian@gmail.com', true, true)
-      		slackNotifier {
-		  notifyAborted(true)
-		  notifyEveryFailure(true)
-		  notifyNotBuilt(false)
-		  notifyUnstable(false)
-		  notifyBackToNormal(true)
-		  notifySuccess(false)
-		  notifyRepeatedFailure(false)
-		  startNotification(false)
-		  includeTestSummary(false)
-		  includeCustomMessage(false)
-		  customMessage(null)
-		  sendAs(null)
-		  commitInfoChoice('NONE')
-		  teamDomain(null)
-		  authToken(null)
-        	}
-    	}
+    	stringParam('Nombre', defaultValue = 'Julio', description = 'Parametro de cadena para el Job Booleno')
+        choiceParam('Planeta', ['Mercurio', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+        booleanParam('Agent', false)
+    }
+    triggers {
+      cron('H/20 * * * *')
+    }
+    steps {
+      shell("sh jobscript.sh")
+    }
+  	       
+    publishers {
+      mailer('juliocvd9891@gmail.com julio.vargas.condor@gmail.com', true, true)
+      slackNotifier {
+        notifyAborted(true)
+		notifyEveryFailure(true)
+		notifyNotBuilt(false)
+		notifyUnstable(false)
+		notifyBackToNormal(true)
+		notifySuccess(true)
+		notifyRepeatedFailure(false)
+		startNotification(false)
+		includeTestSummary(false)
+		includeCustomMessage(false)
+		customMessage(null)
+		sendAs(null)
+		commitInfoChoice('NONE')
+		teamDomain(null)
+		authToken(null)
+      }
+    }
 }
